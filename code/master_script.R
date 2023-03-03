3074d568692947b689c4bc1d1d64bfe4535b7268
a0981906660 <a0981906660@gmail.com>
2023-03-02 21:18:45 -0600

    Add master script

---
+++ code/master_script.R
@@ -0,0 +1,26 @@
+#' @title Master Script
+
+#' @section Pull data from several resources (hand collection)
+
+#' @section constructing sentiment index
+
+#' @section data cleaning
+
+#' @section  estimation of SVAR
+
+# plot time series
+
+# estimate 
+
+# estimate IRF
+
+# plot IRF
+
+# estimate Variance Decomposition
+
+# tabulate VD
+
+# estimate Historical Decomposition
+
+# plot HD
+
